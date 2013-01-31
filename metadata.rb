cd58d11342dd2141c72869ef6c4303cd4ed9cafd
ashmere <ashmere@gmail.com>
2013-01-31 11:59:31 +0000

    making it depend of nodejs cookbook to support all install methods for that

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,9 @@ maintainer_email "jon@blankpad.net"
 license          "Apache 2.0"
 description      "Installs/Configures statsd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
 
 depends "build-essential"
 depends "git"
+depends "nodejs"
 supports "ubuntu"
